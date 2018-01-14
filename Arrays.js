3c39909770f420df848ef29d602f3b4b8a2cda81
Gaston Mollica <mollica.gaston@gmail.com>
2018-01-15 01:47:33 +1000

    basics complete

--- Arrays.js
+++ Arrays.js
@@ -3,3 +3,9 @@ let houseMates = ['Lockie', 'Chris', 'Misha', 'Zach', 'Hayley']
 houseMates.push('Gaston')
 
 console.log(houseMates)
+
+houseMates.pop('Misha')
+
+console.log(houseMates)
+
+//.shift removes the first item in an array
